79fb9bbddf67c21c2530dae23905289cd1c2dbcf
Aqib Ali <aaqibalii32@gmail.com>
2020-10-11 19:50:46 +0500

    Site changes

--- application/config/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$route['default_controller'] = 'dashboard/index';
-
-$route['404_override'] = 'My404';
-$route['translate_uri_dashes'] = FALSE;
-$route['my-chart'] = "Dashboard";
